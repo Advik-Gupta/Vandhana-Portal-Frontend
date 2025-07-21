181c47f35c3e840e978f75604fa8dc76a04d8954
Advik-Gupta <advikguptadev@gmail.com>
2025-07-21 14:23:49 +0530

    fix

--- src/components/api/client.js
+++ src/components/api/client.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const backEndUrl =
-  import.meta.env.VITE_BACKEND_URL ||
-  "https://vandhana-portal-backend.onrender.com";
+const backEndUrl = "https://vandhana-portal-backend.onrender.com";
 
 const client = axios.create({
   baseURL: `${backEndUrl}/api/v1`,
